3c2f123f0e91bf25fe160d6489f41bc4d816d3fe
leontogithub <2815986659@qq.com>
2022-07-03 16:57:13 +0800

    3.0

--- src/main/java/com/zcw/crm/workbench/domain/GitClass.java
+++ src/main/java/com/zcw/crm/workbench/domain/GitClass.java
@@ -8,4 +8,11 @@ public class GitClass {
 
     public GitClass() {
     }
+
+
+    public GitClass(String id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
 }
